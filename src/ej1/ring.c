393cac5c9233970d971ce0eed415291c0a9055b4
Tiziano Martín <tmartinbernal@udesa.edu.ar>
2024-05-07 20:24:19 -0300

    final version

--- src/ej1/ring.c
+++ src/ej1/ring.c
@@ -4,23 +4,65 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+/*
+Obtiene los índices de lectura y escritura para un buffer circular.
+
+Parámetros:
+- n: Tamaño total del buffer circular.
+- s: Índice de inicio del buffer circular.
+- i: Índice actual en el buffer circular.
+- indice_lectura: Puntero al índice de lectura que se actualizará.
+- indice_escritura: Puntero al índice de escritura que se actualizará.
+*/
 void get_index(int n, int s, int i, int *indice_lectura, int *indice_escritura){
-	if (i == s){
-		*indice_lectura = 0;
-		*indice_escritura = 1;
-	}
-	if (i > s){
-		*indice_lectura = i - s;
-		*indice_escritura = i - s + 1;
-	}
-	if (i < s){
-		*indice_lectura = n - s + i;
-		*indice_escritura = n - s + i + 1;
-	}
+    if (i == s){
+        *indice_lectura = 0;
+        *indice_escritura = 1;
+    }
+    if (i > s){
+        *indice_lectura = i - s;
+        *indice_escritura = i - s + 1;
+    }
+    if (i < s){
+        *indice_lectura = n - s + i;
+        *indice_escritura = n - s + i + 1;
+    }
 }
 
-int mod (int a, int b){
-	return (a % b + b) % b;
+/*
+Calcula el módulo de un número.
+
+Parámetros:
+- a: El dividendo.
+- b: El divisor.
+
+Retorna:
+El resultado de la operación (a % b + b) % b.
+*/
+int mod(int a, int b) {
+    return (a % b + b) % b;
+}
+
+/*
+Imprime un mensaje según el estado del proceso hijo.
+
+Parámetros:
+- i: El índice del proceso hijo.
+- num: El número recibido por el proceso hijo.
+- indice_lectura: El índice de lectura del buffer circular.
+- indice_escritura: El índice de escritura del buffer circular.
+- n: El tamaño total del buffer circular.
+*/
+void print_message(int i, int num, int indice_lectura, int indice_escritura, int n) {
+    if (n == 1) {
+        printf("soy el hijo %i, recibi un %i del padre, lo incremento a %i y se lo mando al padre\n", i, num, num + 1);
+    } else if (indice_lectura == 0) {
+        printf("soy el hijo %i, recibi un %i del padre, lo incremento a %i y se lo mando al hijo %i\n", i, num, num + 1, mod(i + 1, n));
+    } else if (indice_escritura == n) {
+        printf("soy el hijo %i, recibi un %i del hijo %i, lo incremento a %i y se lo mando al padre\n", i, num, mod(i - 1, n), num + 1);
+    } else {
+        printf("soy el hijo %i, recibi un %i del hijo %i, lo incremento a %i y se lo mando al hijo %i\n", i, num, mod(i - 1, n), num + 1, mod(i + 1, n));
+    }
 }
 
 int main(int argc, char **argv)
@@ -40,37 +82,50 @@ int main(int argc, char **argv)
 		printf("s debe ser menor a n y mayor o igual a 0\n");
 		exit(0);
 	}
+
 	printf("Se crearan %i procesos, se enviará el caracter %i desde proceso %i\n", n, c, s);
 
-	int pipes[n + 1][2];
+	int pipes[n + 1][2]; // El primer pipe comunica al padre con el primer hijo, el último pipe comunica al último hijo con el padre
 	for (int i = 0; i <= n; i++){
-		pipe(pipes[i]);
+		if (pipe(pipes[i]) == -1){
+			perror("pipe");
+			exit(EXIT_FAILURE);
+		}
 	}
+
+	pid_t pids[n];
 
 	for (int i = 0; i < n; i++){
 		pid = fork();
+		pids[i] = pid;
+
+		if (pid == -1){
+			perror("fork");
+			exit(EXIT_FAILURE);
+		}
+
 		if (pid == 0){ // hijo
 			int indice_lectura;
 			int indice_escritura;
 			get_index(n, s, i, &indice_lectura, &indice_escritura);
 
 			int num;
-			read(pipes[indice_lectura][0], &num, sizeof(int));
 
-			if (indice_lectura == 0 && n != 1){
-				printf("soy el hijo %i, recibi un %i del padre, lo incremento a %i y se lo mando al hijo %i\n", i, num, num + 1, mod(i + 1, n));
+			for (int j = 0; j <= n; j++){
+				if (j != indice_lectura && j != indice_escritura){
+					close(pipes[j][0]);
+					close(pipes[j][1]);
+				}
 			}
-			if (indice_escritura == n && n != 1){
-				printf("soy el hijo %i, recibi un %i del hijo %i, lo incremento a %i y se lo mando al padre\n", i, num, mod(i - 1, n), num + 1);
-			}
-			if (indice_lectura != 0 && indice_escritura != n){
-				printf("soy el hijo %i, recibi un %i del hijo %i, lo incremento a %i y se lo mando al hijo %i\n", i, num, mod(i - 1, n), num + 1, mod(i + 1, n));
-			}
-			if (n == 1){
-				printf("soy el hijo %i, recibi un %i del padre, lo incremento a %i y se lo mando al padre\n", i, num, num + 1);
-			}
+
+			close(pipes[indice_lectura][1]);
+			close(pipes[indice_escritura][0]);
+
+			read(pipes[indice_lectura][0], &num, sizeof(int));
+			print_message(i, num, indice_lectura, indice_escritura, n);
 			num++;
 			write(pipes[indice_escritura][1], &num, sizeof(int));
+
 			close(pipes[indice_lectura][0]);
 			close(pipes[indice_escritura][1]);
 			return 0;
@@ -81,19 +136,30 @@ int main(int argc, char **argv)
 			}
 		}
 	}
-	if (pid != 0){
-		for (int i = 0; i <= n; i++){
+
+	// cierro los pipes en el padre
+	for (int i = 0; i <= n; i++){
+		if (i == n){
 			close(pipes[i][1]);
 		}
-		for (int i = 0; i < n; i++){
-			wait(&status);
-		}
-		int num;
-		read(pipes[n][0], &num, sizeof(int));
-		printf("soy el padre, recibi un %i\n", num);
-		for (int i = 0; i <= n; i++){
+		else{
 			close(pipes[i][0]);
+			close(pipes[i][1]);
 		}
 	}
+
+	// espero a que terminen los hijos
+	for (int i = 0; i < n; i++){
+		if (waitpid(pids[i], &status, 0) == -1){
+			perror("waitpid");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	int num;
+	read(pipes[n][0], &num, sizeof(int));
+	printf("soy el padre, recibi un %i\n", num);
+	close(pipes[n][0]);
+
 	return 0;
 }
--- src/ej2/shell.c
+++ src/ej2/shell.c
@@ -2,12 +2,117 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/types.h>
 #include <string.h>
+#include <stdbool.h>
+
 
 #define MAX_COMMANDS 200
+#define MAX_ARGS 256
+
+/*
+Removes double and single quotes from a string.
+
+Parameters:
+- string: The string from which quotes will be removed.
+*/
+void remove_quotes(char *string) {
+    int i, j = 0;
+    for (i = 0; string[i] != '\0'; i++) {
+        if (string[i] != '"' && string[i] != '\'') {
+            string[j++] = string[i];
+        }
+    }
+    string[j] = '\0';
+}
+
+/*
+Tokenizes a command into an array of commands.
+
+Parameters:
+- command: The command to be tokenized.
+- commands: The array of commands.
+- command_count: The number of commands in the array.
+*/
+void tokenize(char *command, char *commands[], int *command_count){
+    char *token = strtok(command, "|");
+
+    while (token != NULL){
+        commands[(*command_count)++] = token;
+        token = strtok(NULL, "|");
+        }
+}
+
+/*
+Creates pipes for the number of commands.
+
+Parameters:
+- pipes: The array of pipes.
+- command_count: The number of commands.
+
+Returns:
+- 0 if the pipes were created successfully.
+- -1 if an error occurred.
+*/
+int create_pipes(int pipes[][2], int command_count){
+    for (int i = 0; i < command_count; i++){
+        if (pipe(pipes[i]) == -1){
+            return -1;
+            }
+        }
+    return 0;
+}
+
+/*
+Redirects the standard input and output for the child processes and closes the pipes.
+
+Parameters:
+- pipes: The array of pipes.
+- command_count: The number of commands.
+- i: The index of the current command.
+*/
+void redirect_and_close(int pipes[][2], int command_count, int i){
+    if (i == 0 && command_count > 1){
+        for (int j = 0; j < command_count; j++){
+            if (j != 0){
+                close(pipes[j][0]);
+                close(pipes[j][1]);
+                }
+            }
+        close(pipes[i][0]);
+        dup2(pipes[i][1], STDOUT_FILENO);
+        }
+    else if (i == command_count - 1 && command_count > 1){
+        for (int j = 0; j < command_count; j++){
+            if (j != i - 1){
+                close(pipes[j][0]);
+                close(pipes[j][1]);
+                }
+            }
+        close(pipes[i - 1][1]);
+        dup2(pipes[i - 1][0], STDIN_FILENO);
+        }
+    else if (command_count > 1){
+        for (int j = 0; j < command_count; j++){
+            if (j != i - 1 && j != i){
+                close(pipes[j][0]);
+                close(pipes[j][1]);
+                }
+            }
+        close(pipes[i - 1][1]);
+        close(pipes[i][0]);
+        dup2(pipes[i - 1][0], STDIN_FILENO);
+        dup2(pipes[i][1], STDOUT_FILENO);
+        close(pipes[i - 1][0]);
+        close(pipes[i][1]);
+        }
+    else{
+        close(pipes[i][0]);
+        close(pipes[i][1]);
+        }
+}
 
 int main() {
-
     char command[256];
     char *commands[MAX_COMMANDS];
     int command_count = 0;
@@ -15,33 +120,81 @@ int main() {
     while (1) 
     {
         printf("Shell> ");
-        
-        /*Reads a line of input from the user from the standard input (stdin) and stores it in the variable command */
         fgets(command, sizeof(command), stdin);
-        
-        /* Removes the newline character (\n) from the end of the string stored in command, if present. 
-           This is done by replacing the newline character with the null character ('\0').
-           The strcspn() function returns the length of the initial segment of command that consists of 
-           characters not in the string specified in the second argument ("\n" in this case). */
         command[strcspn(command, "\n")] = '\0';
 
-        /* Tokenizes the command string using the pipe character (|) as a delimiter using the strtok() function. 
-           Each resulting token is stored in the commands[] array. 
-           The strtok() function breaks the command string into tokens (substrings) separated by the pipe character |. 
-           In each iteration of the while loop, strtok() returns the next token found in command. 
-           The tokens are stored in the commands[] array, and command_count is incremented to keep track of the number of tokens found. */
-        char *token = strtok(command, "|");
-        while (token != NULL) 
-        {
-            commands[command_count++] = token;
-            token = strtok(NULL, "|");
+        tokenize(command, commands, &command_count);
+
+        int pipes[command_count][2];
+
+        if (create_pipes(pipes, command_count) == -1){
+            perror("pipe");
+            exit(EXIT_FAILURE);
+            }
+
+        pid_t pids[command_count];
+
+        for (int i = 0; i < command_count; i++){
+            pid_t pid = fork();
+            pids[i] = pid;
+
+            if (pid == -1){
+                perror("fork");
+                exit(EXIT_FAILURE);
+                }
+
+            else if (pid == 0){ // caso hijo
+
+                redirect_and_close(pipes, command_count, i);
+
+                // creo un arreglo para guardar el comando y los argumentos
+                char *args[MAX_ARGS];
+                char *command_copy = malloc(strlen(commands[i]) + 1);
+                strcpy(command_copy, commands[i]);
+
+                char *token = strtok(command_copy, " ");
+                int j = 0;
+                while (token != NULL){
+                    args[j++] = token;
+                    token = strtok(NULL, " ");
+                    }
+                args[j] = NULL;
+                
+                // quitar las comillas
+                for (int k = 0; k < j; k++){
+                    remove_quotes(args[k]);
+                }
+                
+                // crear una variable que tenga el tamaño exacto
+                char *args2[j + 1];
+                for (int k = 0; k < j; k++){
+                    args2[k] = args[k];
+                    }
+                args2[j] = NULL;
+
+                if (execvp(args2[0], args2) == -1){
+                    perror("execvp");
+                    exit(EXIT_FAILURE);
+                }
+            }
         }
 
-        /* You should start programming from here... */
-        for (int i = 0; i < command_count; i++) 
-        {
-            printf("Command %d: %s\n", i, commands[i]);
-        }    
+        // cerrar los pipes en el padre
+        for (int i = 0; i < command_count; i++){
+            close(pipes[i][0]);
+            close(pipes[i][1]);
+            }
+
+        for (int i = 0; i < command_count; i++){
+            int status;
+            if (waitpid(pids[i], &status, 0) == -1){
+                perror("waitpid");
+                exit(EXIT_FAILURE);
+                }
+            }
+
+        command_count = 0;
+
     }
     return 0;
 }
